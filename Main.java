88ff8e7ed6a7425036c73003fb894fda003b1bfc
Subhadwip Chakraborty <subhadwipchakraborty@Subhadwips-MacBook-Air.local>
2025-05-10 00:47:17 +0530

    reused sum method

--- Main.java
+++ Main.java
@@ -2,13 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello worlds");
         System.out.println("My name is Khan");
-
         int a  = 10;
         System.out.println(sum(a,a));
-
-    }
-    static int sum(int a, int b) {
-        return a+b;
     }
 
     int sum(int a, int b) {
