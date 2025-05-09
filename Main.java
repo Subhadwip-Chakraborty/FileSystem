7274c54ff7d24b9567cebac57db7845e8e0d6aaf
Subhadwip Chakraborty <81144274+Subhadwip-Chakraborty@users.noreply.github.com>
2025-05-10 00:54:08 +0530

    UK-277 used sum method (#3)
    * UK-277 used sum method
    
    * reused sum method
    
    ---------
    
    Co-authored-by: Subhadwip Chakraborty <subhadwipchakraborty@Subhadwips-MacBook-Air.local>

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello worlds");
         System.out.println("My name is Khan");
+        int a  = 10;
+        System.out.println(sum(a,a));
     }
 
     int sum(int a, int b) {
