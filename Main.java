29adf73c10303a3d31aea9696e5e5ed946d28ab3
Subhadwip Chakraborty <subhadwipchakraborty@Subhadwips-MacBook-Air.local>
2025-05-10 00:41:57 +0530

    UK-277 used sum method

--- Main.java
+++ Main.java
@@ -2,6 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello worlds");
         System.out.println("My name is Khan");
+
+        int a  = 10;
+        System.out.println(sum(a,a));
+
+    }
+    static int sum(int a, int b) {
+        return a+b;
     }
 
     int sum(int a, int b) {
